0d63c0066028ad288ace56bfaea0981e144e9e14
EinSatzMitX <PeterFreder@outlook.de>
2025-01-30 21:34:42 +0100

    still working on the skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+mod bus;
+use bus::BUS;
+mod cpu;
+use cpu::CPU;
+
+use std::rc::Rc;
+use std::cell::RefCell;
+
 fn main() {
-    println!("Hello, world!");
+    /* Don't mess with this if you don't know what you're doing */
+
+    // Create BUS and wrap it in Rc<RefCell>
+    let bus = Rc::new(RefCell::new(BUS::new()));
+    
+    // Create CPU and pass it a reference to the BUS
+    let cpu = Rc::new(RefCell::new(CPU::new(Rc::clone(&bus))));
+    
+    // Link the CPU to the BUS (Weak reference)
+    bus.borrow_mut().link_cpu(Rc::downgrade(&cpu));
 }
